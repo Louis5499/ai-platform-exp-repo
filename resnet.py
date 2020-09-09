03dfab4f5803442372f4dc57ca362ad03e976793
Mencher Chiang <louis@9dynamics.com.tw>
2020-09-09 16:50:11 +0800

    renew resnet-model

--- resnet.py
+++ resnet.py
@@ -1,230 +1,99 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Densenet Training."""
+from tensorflow import Tensor
+from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
+                                    Add, AveragePooling2D, Flatten, Dense
+from tensorflow.keras.models import Model
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+def relu_bn(inputs: Tensor) -> Tensor:
+    relu = ReLU()(inputs)
+    bn = BatchNormalization()(relu)
+    return bn
 
-from absl import app
-from absl import flags
-import tensorflow as tf
-from tensorflow_examples.models.densenet import densenet
-from tensorflow_examples.models.densenet import utils
+def residual_block(x: Tensor, downsample: bool, filters: int, kernel_size: int = 3) -> Tensor:
+    y = Conv2D(kernel_size=kernel_size,
+               strides= (1 if not downsample else 2),
+               filters=filters,
+               padding="same")(x)
+    y = relu_bn(y)
+    y = Conv2D(kernel_size=kernel_size,
+               strides=1,
+               filters=filters,
+               padding="same")(y)
 
-FLAGS = flags.FLAGS
+    if downsample:
+        x = Conv2D(kernel_size=1,
+                   strides=2,
+                   filters=filters,
+                   padding="same")(x)
+    out = Add()([x, y])
+    out = relu_bn(out)
+    return out
 
-# if additional flags are needed, define it here.
-flags.DEFINE_integer('num_gpu', 1, 'Number of GPUs to use')
+def create_res_net():
+    
+    inputs = Input(shape=(32, 32, 3))
+    num_filters = 64
+    
+    t = BatchNormalization()(inputs)
+    t = Conv2D(kernel_size=3,
+               strides=1,
+               filters=num_filters,
+               padding="same")(t)
+    t = relu_bn(t)
+    
+    num_blocks_list = [2, 5, 5, 2]
+    for i in range(len(num_blocks_list)):
+        num_blocks = num_blocks_list[i]
+        for j in range(num_blocks):
+            t = residual_block(t, downsample=(j==0 and i!=0), filters=num_filters)
+        num_filters *= 2
+    
+    t = AveragePooling2D(4)(t)
+    t = Flatten()(t)
+    outputs = Dense(10, activation='softmax')(t)
+    
+    model = Model(inputs, outputs)
 
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
 
-class Train(object):
-  """Train class.
-  Args:
-    epochs: Number of epochs
-    enable_function: If True, wraps the train_step and test_step in tf.function
-    model: Densenet model.
-    batch_size: Batch size.
-    strategy: Distribution strategy in use.
-  """
-
-  def __init__(self, epochs, enable_function, model, batch_size, strategy):
-    self.epochs = epochs
-    self.batch_size = batch_size
-    self.enable_function = enable_function
-    self.strategy = strategy
-    self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction=tf.keras.losses.Reduction.NONE)
-    self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.1,
-                                             momentum=0.9, nesterov=True)
-    self.train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(
-        name='train_accuracy')
-    self.test_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(
-        name='test_accuracy')
-    self.test_loss_metric = tf.keras.metrics.Sum(name='test_loss')
-    self.model = model
-
-  def decay(self, epoch):
-    if epoch < 150:
-      return 0.1
-    if epoch >= 150 and epoch < 225:
-      return 0.01
-    if epoch >= 225:
-      return 0.001
-
-  def compute_loss(self, label, predictions):
-    loss = tf.reduce_sum(self.loss_object(label, predictions)) * (
-        1. / self.batch_size)
-    loss += (sum(self.model.losses) * 1. / self.strategy.num_replicas_in_sync)
-    return loss
-
-  def train_step(self, inputs):
-    """One train step.
-    Args:
-      inputs: one batch input.
-    Returns:
-      loss: Scaled loss.
-    """
-
-    image, label = inputs
-    with tf.GradientTape() as tape:
-      predictions = self.model(image, training=True)
-      loss = self.compute_loss(label, predictions)
-    gradients = tape.gradient(loss, self.model.trainable_variables)
-    self.optimizer.apply_gradients(zip(gradients,
-                                       self.model.trainable_variables))
-
-    self.train_acc_metric(label, predictions)
-    return loss
-
-  def test_step(self, inputs):
-    """One test step.
-    Args:
-      inputs: one batch input.
-    """
-    image, label = inputs
-    predictions = self.model(image, training=False)
-
-    unscaled_test_loss = self.loss_object(label, predictions) + sum(
-        self.model.losses)
-
-    self.test_acc_metric(label, predictions)
-    self.test_loss_metric(unscaled_test_loss)
-
-  def custom_loop(self, train_dist_dataset, test_dist_dataset,
-                  strategy):
-    """Custom training and testing loop.
-    Args:
-      train_dist_dataset: Training dataset created using strategy.
-      test_dist_dataset: Testing dataset created using strategy.
-      strategy: Distribution strategy.
-    Returns:
-      train_loss, train_accuracy, test_loss, test_accuracy
-    """
-
-    def distributed_train_epoch(ds):
-      total_loss = 0.0
-      num_train_batches = 0.0
-      for one_batch in ds:
-        per_replica_loss = strategy.run(self.train_step, args=(one_batch,))
-        total_loss += strategy.reduce(
-            tf.distribute.ReduceOp.SUM, per_replica_loss, axis=None)
-        num_train_batches += 1
-      return total_loss, num_train_batches
-
-    def distributed_test_epoch(ds):
-      num_test_batches = 0.0
-      for one_batch in ds:
-        strategy.run(self.test_step, args=(one_batch,))
-        num_test_batches += 1
-      return self.test_loss_metric.result(), num_test_batches
-
-    if self.enable_function:
-      distributed_train_epoch = tf.function(distributed_train_epoch)
-      distributed_test_epoch = tf.function(distributed_test_epoch)
-
-    for epoch in range(self.epochs):
-      self.optimizer.learning_rate = self.decay(epoch)
-
-      train_total_loss, num_train_batches = distributed_train_epoch(
-          train_dist_dataset)
-      test_total_loss, num_test_batches = distributed_test_epoch(
-          test_dist_dataset)
-
-      template = ('Epoch: {}, Train Loss: {}, Train Accuracy: {}, '
-                  'Test Loss: {}, Test Accuracy: {}')
-
-      print(
-          template.format(epoch,
-                          train_total_loss / num_train_batches,
-                          self.train_acc_metric.result(),
-                          test_total_loss / num_test_batches,
-                          self.test_acc_metric.result()))
-
-      if epoch != self.epochs - 1:
-        self.train_acc_metric.reset_states()
-        self.test_acc_metric.reset_states()
-
-    return (train_total_loss / num_train_batches,
-            self.train_acc_metric.result().numpy(),
-            test_total_loss / num_test_batches,
-            self.test_acc_metric.result().numpy())
+    return model
 
 
-def run_main(argv):
-  """Passes the flags to main.
-  Args:
-    argv: argv
-  """
-  del argv
-  kwargs = utils.flags_dict()
-  kwargs.update({'num_gpu': FLAGS.num_gpu})
-  main(**kwargs)
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+import datetime
+import os
 
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, y_train = x_train[:100], y_train[:100]
+x_test, y_test = x_test[:100], y_test[:100]
 
-def main(epochs,
-         enable_function,
-         buffer_size,
-         batch_size,
-         mode,
-         growth_rate,
-         output_classes,
-         depth_of_model=None,
-         num_of_blocks=None,
-         num_layers_in_each_block=None,
-         data_format='channels_last',
-         bottleneck=True,
-         compression=0.5,
-         weight_decay=1e-4,
-         dropout_rate=0.,
-         pool_initial=False,
-         include_top=True,
-         train_mode='custom_loop',
-         data_dir=None,
-         num_gpu=1):
+strategy = Tensor.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-  devices = ['/device:GPU:{}'.format(i) for i in range(num_gpu)]
-  strategy = tf.distribute.MirroredStrategy(devices)
+BATCH_SIZE_PER_REPLICA = 64
+BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
 
-  train_dataset, test_dataset, _ = utils.create_dataset(
-      buffer_size, batch_size, data_format, data_dir)
+with strategy.scope():
 
-  with strategy.scope():
-    model = densenet.DenseNet(
-        mode, growth_rate, output_classes, depth_of_model, num_of_blocks,
-        num_layers_in_each_block, data_format, bottleneck, compression,
-        weight_decay, dropout_rate, pool_initial, include_top)
+    model = create_res_net() # or create_plain_net()
+    model.summary()
 
-    trainer = Train(epochs, enable_function, model, batch_size, strategy)
+    # timestr = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    # name = 'cifar-10_res_net_30-'+timestr # or 'cifar-10_plain_net_30-'+timestr
 
-    train_dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
-    test_dist_dataset = strategy.experimental_distribute_dataset(test_dataset)
+    # checkpoint_path = "checkpoints/"+name+"/cp-{epoch:04d}.ckpt"
+    # checkpoint_dir = os.path.dirname(checkpoint_path)
+    # os.system('mkdir {}'.format(checkpoint_dir))
 
-    print('Training...')
-    if train_mode == 'custom_loop':
-      return trainer.custom_loop(train_dist_dataset,
-                                 test_dist_dataset,
-                                 strategy)
-    elif train_mode == 'keras_fit':
-      raise ValueError(
-          '`tf.distribute.Strategy` does not support subclassed models yet.')
-    else:
-      raise ValueError(
-          'Please enter either "keras_fit" or "custom_loop" as the argument.')
-
-
-if __name__ == '__main__':
-  utils.define_densenet_flags()
-  app.run(run_main)
+    model.fit(
+        x=x_train,
+        y=y_train,
+        epochs=1000,
+        verbose=1,
+        validation_data=(x_test, y_test),
+        batch_size=BATCH_SIZE
+    )
